f6f20b2c89a8caa608ab0be5961a7c990d1716c3
x1nett <132476761+x1nett@users.noreply.github.com>
2025-05-29 10:46:31 +0300

    Update Program.cs

--- parprog_lab3/Program.cs
+++ parprog_lab3/Program.cs
@@ -1,33 +1,38 @@
-﻿using System;
+using System;
 using System.Threading;
 
-namespace ProducerConsumerMulti
+namespace ProducerConsumerArray
 {
     class Program
     {
         static void Main(string[] args)
         {
             int storageSize = 5;
-            int[] producerItems = { 5, 3, 4 }; 
-            int[] consumerItems = { 6, 6 };    
+
+            string[][] producerData = {
+                new string[] { "A1", "A2", "A3" },
+                new string[] { "B1", "B2" },
+                new string[] { "C1", "C2", "C3", "C4" }
+            };
+
+            int[] consumerNeeds = { 4, 5 }; 
 
             Storage storage = new Storage(storageSize);
 
-           
-            for (int i = 0; i < producerItems.Length; i++)
+    
+            for (int i = 0; i < producerData.Length; i++)
             {
-                int id = i;
-                int items = producerItems[i];
-                Producer producer = new Producer(id, items, storage);
-                new Thread(producer.Run).Start();
+                Producer producer = new Producer(i, producerData[i], storage);
+                Thread thread = new Thread(producer.Run);
+                thread.Start();
             }
 
-            for (int i = 0; i < consumerItems.Length; i++)
+            // Стартуємо потоки споживачів
+            for (int i = 0; i < consumerNeeds.Length; i++)
             {
-                int id = i;
-                int items = consumerItems[i];
-                Consumer consumer = new Consumer(id, items, storage);
-                new Thread(consumer.Run).Start();
+                Consumer consumer = new Consumer(i, consumerNeeds[i], storage);
+                Thread thread = new Thread(consumer.Run);
+                thread.Start();
             }
 
             Console.ReadKey();
