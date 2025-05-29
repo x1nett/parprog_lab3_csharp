d75a2f4be091890834df37194f45d0ab64ce2d7e
x1nett <132476761+x1nett@users.noreply.github.com>
2025-05-29 10:44:50 +0300

    Update Storage.cs

--- parprog_lab3/Storage.cs
+++ parprog_lab3/Storage.cs
@@ -1,23 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 
-namespace ProducerConsumerMulti
+namespace ProducerConsumerArray
 {
     public class Storage
     {
         private readonly int capacity;
-        private readonly List<string> storage = new List<string>();
-
-        private readonly Semaphore access = new Semaphore(1, 1);
+        private readonly List<string> buffer;
+        private readonly Semaphore access;
         private readonly Semaphore full;
         private readonly Semaphore empty;
 
         public Storage(int capacity)
         {
             this.capacity = capacity;
-            full = new Semaphore(capacity, capacity); 
-            empty = new Semaphore(0, capacity);       
+            buffer = new List<string>();
+            access = new Semaphore(1, 1);
+            full = new Semaphore(capacity, capacity);
+            empty = new Semaphore(0, capacity);
         }
 
         public void Add(string item, int producerId)
@@ -25,25 +26,21 @@ namespace ProducerConsumerMulti
             full.WaitOne();
             access.WaitOne();
 
-            storage.Add(item);
+            buffer.Add(item);
             Console.WriteLine($"[Producer {producerId}] Produced: {item}");
 
             access.Release();
             empty.Release();
         }
 
-        public string Remove(int consumerId)
+        public string Take(int consumerId)
         {
             empty.WaitOne();
             access.WaitOne();
 
-            string item = null;
-            if (storage.Count > 0)
-            {
-                item = storage[0];
-                storage.RemoveAt(0);
-                Console.WriteLine($"\t[Consumer {consumerId}] Consumed: {item}");
-            }
+            string item = buffer[0];
+            buffer.RemoveAt(0);
+            Console.WriteLine($"\t[Consumer {consumerId}] Consumed: {item}");
 
             access.Release();
             full.Release();
